05170d57b9ffdfb02233a8626af573c0e7bc752e
TerryDutton <t_dutton85@hotmail.com>
2018-07-12 15:45:20 +0100

    Removed unnecessary else-block.

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ exports.jobLister = function(str){
     if (acc.includes('Error') || acc.includes(jobInfo) || jobInfo === '') return acc; 
     if (jobInfo.length === 1) return acc + jobInfo; 
 
-    else {
-      const [job, dependsOn] = [...jobInfo];
-      if (job === dependsOn) return 'Error: sequence contains a job with itself as a dependency.';
-      if ([job, dependsOn].every(letter => !acc.includes(letter))) acc += dependsOn + job;
-      else if (acc.includes(job)) acc = dependsOn + acc; 
-      else acc += job;
-      return acc.indexOf(dependsOn) === acc.lastIndexOf(dependsOn) ? acc : 'Error: sequence contains a circular set of dependencies.';
-    }
+    const [job, dependsOn] = [...jobInfo];
+    if (job === dependsOn) return 'Error: sequence contains a job with itself as a dependency.';
+    if ([job, dependsOn].every(letter => !acc.includes(letter))) acc += dependsOn + job;
+    else if (acc.includes(job)) acc = dependsOn + acc; 
+    else acc += job;
+    return acc.indexOf(dependsOn) === acc.lastIndexOf(dependsOn) ? acc : 'Error: sequence contains a circular set of dependencies.';
   }, '');
 }
