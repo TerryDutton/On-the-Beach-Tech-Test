14e8adf8dcdf6d5b8ba38b942a4d40b640916017
TerryDutton <t_dutton85@hotmail.com>
2018-07-12 12:16:02 +0100

    Function lists all jobs and correctly orders them based on dependencies.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 exports.jobLister = function(str){
-  return str.replace(/\W/g, '');
+  const arrayOfJobs = str.split('\n').map(job => job.replace(/\W/g, ''));
+
+  const {jobs, dependentPairs} = arrayOfJobs.reduce((acc, jobStr) => {
+    acc.jobs += jobStr[0] || '';
+    if (jobStr.length > 1) acc.dependentPairs.push(jobStr);
+    return acc; 
+  }, {jobs: '', dependentPairs: []});
+
+  return dependentPairs.reduce((jobList, [job, dep]) => {
+    const regexp = new RegExp(`[${job}${dep}]`, 'g'); 
+    return jobList.replace(regexp, match => {
+      if (match === job) return `${dep}${job}`; 
+      return '';
+    });
+  }, jobs);
 }
--- index.spec.js
+++ index.spec.js
@@ -14,4 +14,27 @@ describe('jobLister', () => {
     c =>`;
     expect(jobLister(input)).to.equal('abc');
   });
+
+  it('when passed a list of jobs with dependencies, returns a string of jobs with dependencies listed after the jobs they depend on.', () => {
+    let input = 
+   `a =>
+    b => c
+    c =>`;
+    expect(jobLister(input)).to.equal('acb');
+
+    input = 
+   `a => 
+    b => c
+    c => f
+    d => a
+    e => b
+    f => `;
+    const output = jobLister(input); 
+  const expectedSeqs = [/f.*c/, /c.*b/, /b.*e/, /a.*d/];
+    /*We expect f before c, c before b, b before e and a before d, but they might have other letters between them, 
+    so simply checking for pairs of letters won't work.*/
+    expect(expectedSeqs.every(seq => seq.test(output))).to.be.true;
+    expect([...'abcdef'].every(letter => output.includes(letter))).to.be.true;
+    expect(output.length).to.equal(6);  
+  });
 });
